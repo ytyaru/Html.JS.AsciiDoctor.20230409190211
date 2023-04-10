abed9e2e3c6be2fe3af981892da529b6b8737202
ytyaru <yttry0@gmail.com>
2023-04-10 11:09:44 +0900

    fetchでファイル取得するも相対パス使えず

---
+++ docs/2/js/main.js
@@ -0,0 +1,78 @@
+window.addEventListener('DOMContentLoaded', async(event) => {
+    console.log('DOMContentLoaded!!');
+    let asciidoctor = Asciidoctor()
+    function show(ad) {
+        console.log(ad)
+        const html = asciidoctor.convert(ad)
+        console.log(html)
+        document.getElementById('content').innerHTML = html
+    }
+    //const res = await fetch(`https://localhost/2/ad/index.ad`)
+    fetch(`index.ad`)
+    .then(res=>res.text())
+    .then(ad=>{show(ad)})
+    .catch(err=>console.error(err))
+    /*
+    .then(ad=>{
+        console.log(ad)
+        const html = asciidoctor.convert(ad)
+        console.log(html)
+        document.getElementById('content').innerHTML = html
+    }).catch(err=>console.error(err))
+    */
+    fetch(`ad/index.ad`).then(res=>res.text()).then(ad=>{show(ad)}).catch(e=>console.error(e))
+    fetch(`2/ad/index.ad`).then(res=>res.text()).then(ad=>{show(ad)}).catch(e=>console.error(e))
+    fetch(`doc/2/ad/index.ad`).then(res=>res.text()).then(ad=>{show(ad)}).catch(e=>console.error(e))
+    fetch(`./ad/index.ad`).then(res=>res.text()).then(ad=>{show(ad)}).catch(e=>console.error(e))
+    fetch(`./2/ad/index.ad`).then(res=>res.text()).then(ad=>{show(ad)}).catch(e=>console.error(e))
+    fetch(`./doc/2/ad/index.ad`).then(res=>res.text()).then(ad=>{show(ad)}).catch(e=>console.error(e))
+    const res = await fetch(`2/ad/index.ad`)
+    console.log(res)
+    const ad = await res.text()
+    console.log(ad)
+    let html = asciidoctor.convert(ad)
+    document.getElementById('content').innerHTML = html
+    /*
+    try { var doc = asciidoctor.loadFile('ad/index.ad');  console.log(doc); }
+    catch(e) { console.error(e) }
+    try { var doc = asciidoctor.convertFile('ad/index.ad');  console.log(doc); }
+    catch(e) { console.error(e) }
+    try { var doc = asciidoctor.loadFile('ad/index.ad', { to_file: false });  console.log(doc); }
+    catch(e) { console.error(e) }
+    try { var doc = asciidoctor.convertFile('ad/index.ad', { to_file: false });  console.log(doc); }
+    catch(e) { console.error(e) }
+    try { var doc = asciidoctor.loadFile('ad/index.ad', { to_file: false, safe:'UNSAFE' });  console.log(doc); }
+    catch(e) { console.error(e) }
+    try { var doc = asciidoctor.convertFile('ad/index.ad', { to_file: false, safe:'UNSAFE' });  console.log(doc); }
+    catch(e) { console.error(e) }
+    try { var doc = asciidoctor.loadFile('ad/index.ad', { to_file: false, standalone: true, safe:'UNSAFE' });  console.log(doc); }
+    catch(e) { console.error(e) }
+    try { var doc = asciidoctor.convertFile('ad/index.ad', { to_file: false, standalone: true, safe:'UNSAFE' });  console.log(doc); }
+    catch(e) { console.error(e) }
+    */
+
+    //var doc = asciidoctor.convertFile('ad/index.ad')
+    //var html = asciidoctor.convert('= あいう\n\n== あああ\n\nHello, _Asciidoctor_')
+//    var html = asciidoctor.convert('*This* is Asciidoctor.<br>aaaa', { standalone: true })
+//    console.log(html)
+    /*
+    var doc = asciidoctor.convertFile('/path/to/file.adoc')
+    console.log(doc)
+    var html = asciidoctor.convertFile('/path/to/file.adoc', { to_file: false, standalone: true })
+    console.log(html)
+    var doc = asciidoctor.loadFile('file.adoc')
+    console.log(doc.getDocumentTitle())
+    console.log(doc.getAttributes())
+    doc.convert()
+    var doc = asciidoctor.load('Hello, _Asciidoctor_')
+    */
+    //const ad = `= 見出し\n\n文章。`
+    //const html = asciidoctor.convert(ad)
+    //console.log(html)
+    //document.getElementById('content').innerHTML = html
+
+});
+window.addEventListener('beforeunload', (event) => {
+    console.log('beforeunload!!');
+});
+
